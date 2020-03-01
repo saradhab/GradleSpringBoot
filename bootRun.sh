64aebf9956af7ce1ee9a1714dd46801e72045082
mkdhuligithub <60852930+mkdhuligithub@users.noreply.github.com>
2020-03-01 00:54:59 -0500

    Update bootRun.sh

--- bootRun.sh
+++ bootRun.sh
@@ -1,11 +1,15 @@
 #clean current process running on 8080
+echo "killing current process running on 8080..."
 fuser -n tcp -k 8080
 
 #sleep for 10 seconds to kill the process
 sleep 10
 
 #clean temporary gradle directory
+echo "Removing temporary gradle directory..."
 rm -rf .gradle
 
 #run spring boot in background
+echo "Running gradle script to build the project..."
+cd /var/lib/jenkins/workspace/GradleSpringBoot
 nohup gradle bootRun > bootRun.log 2>&1 &
